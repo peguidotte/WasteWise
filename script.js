933486567acfc7e5554ff569b92dcc248358b40d
PedroGuidotte <peguidotte@gmail.com>
2024-05-31 15:49:00 -0300

    Menu Hamburguer completo
    Adição de um menu hamburguer funcional com media querry

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 //Caso o usuário não mexa o mouse por 3segundos o Header desaparece, porém caso o usuário ainda não tenha scrollado a página isso não acontecerá
 let timer;
 
-document.addEventListener('mousemove', function() {
+document.addEventListener('mousemove', function () {
     clearTimeout(timer);
 
     if (window.scrollY > 0) {
         document.querySelector('header').classList.remove('hide');
 
-        timer = setTimeout(function() {
+        timer = setTimeout(function () {
             document.querySelector('header').classList.add('hide');
         }, 3000);
     }
 });
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     if (window.scrollY === 0) {
         document.querySelector('header').classList.remove('hide');
     }
 });
+
+
+function openMenu() {
+    console.log ("Botão clicado")
+    const itens_nav = document.querySelector('.header_nav_list');
+    if (itens_nav && (itens_nav.style.display === 'none' || !itens_nav.style.display)) {
+        itens_nav.style.display = 'block';
+        itens_nav.style.position = 'absolute';
+        itens_nav.style.top = '100%';
+        itens_nav.style.right = '0';
+        itens_nav.style.textAlign = 'center';
+        itens_nav.style.backgroundColor = 'rgba(227, 227, 227, 0.2)';
+        
+        const navItems = document.querySelectorAll('.header_nav_list li');
+            navItems.forEach(item => {
+                item.style.padding = '20px';
+            });
+
+
+    } else if (itens_nav) {
+        itens_nav.style.display = 'none'
+    }
+}
+
+
